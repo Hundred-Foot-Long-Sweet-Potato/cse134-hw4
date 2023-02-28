0b6946fe4105f25fe20b335d4a59fd0b93506931
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-02-28 00:28:09 -0800

    Custom confirm

--- customdialog.js
+++ customdialog.js
@@ -4,3 +4,9 @@ const alertDia = document.getElementById("alertDialog");
 custmAlert.addEventListener("click", ()=>{
     alertDia.showModal();
 })
+
+const custmConfirm = document.getElementById("customConfirmBtn");
+const confirmDia = document.getElementById("confirmDialog");
+custmConfirm.addEventListener("click", () => {
+    confirmDia.showModal();
+})
