e6f9af81e97b94b720ac14db8dd564f4eb9a7dd6
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-02-28 00:24:11 -0800

    Another typo

--- customdialog.js
+++ customdialog.js
@@ -1,5 +1,5 @@
 
-const custmAlert = document.getElementById("CustomAlertBtn");
+const custmAlert = document.getElementById("customAlertBtn");
 const alertDia = document.getElementById("alertDialog");
 custmAlert.addEventListener("click", ()=>{
     alertDia.showModal();
