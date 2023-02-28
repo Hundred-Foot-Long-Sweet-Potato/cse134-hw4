f801f200ffa59fc357d708b4d555701d3ab07028
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-02-28 00:50:15 -0800

    Custom Prompt

--- customdialog.js
+++ customdialog.js
@@ -21,3 +21,26 @@ confirmYes.addEventListener("click", ()=>{
 confirmNo.addEventListener("click", ()=>{
     document.getElementById("customConfirmOut").innerHTML = "no";
 })
+
+const custmPrompt = document.getElementById("customPromptBtn");
+const promptDia = document.getElementById("promptDialog");
+const promptVal = document.getElementById("customPromptVal");
+const confirmSubmit = document.getElementById("promptYes");
+const confirmCancel = document.getElementById("promptNo");
+let output;
+custmPrompt.addEventListener("click", () =>{
+    promptDia.showModal();
+})
+
+promptVal.addEventListener("change", (e)=>{
+    output = e;
+})
+
+confirmSubmit.addEventListener("click", ()=>{
+    document.getElementById("customPromptOut").innerHTML = output;
+})
+
+confirmCancel.addEventListener("click", ()=>{
+    document.getElementById("customPromptOut").innerHTML = "Canceled!";
+})
+
