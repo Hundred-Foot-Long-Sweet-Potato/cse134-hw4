ffa64791fa2054cb15134c6d253bb906231cce7b
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-02-28 00:21:19 -0800

    Custom Dia 1

---
+++ customdialogue.js
@@ -0,0 +1,6 @@
+
+const custmAlert = document.getElementById("CustomAlertBtn");
+const alertDia = document.getElementById("alertDialog");
+custmAlert.addEventListener("click", ()=>{
+    alertDia.showModal();
+})
