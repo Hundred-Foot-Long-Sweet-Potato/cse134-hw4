cd8b2a05b46bfc1d507fc1fcbeb74743c375156e
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-02-28 00:33:44 -0800

    Added output to custom confirm

--- customdialog.js
+++ customdialog.js
@@ -7,6 +7,18 @@ custmAlert.addEventListener("click", ()=>{
 
 const custmConfirm = document.getElementById("customConfirmBtn");
 const confirmDia = document.getElementById("confirmDialog");
+const outputConfirm = document.getElementById("customConfirmOut");
+const confirmYes = document.getElementById("confirmYes");
+const confirmNo = document.getElementById("confirmNo");
+
 custmConfirm.addEventListener("click", () => {
     confirmDia.showModal();
+})
+
+confirmYes.addEventListener("click", ()=>{
+    outputConfirm.value = "yes";
+})
+
+confirmNo.addEventListener("click", ()=>{
+    outputConfirm.value="no";
 })
