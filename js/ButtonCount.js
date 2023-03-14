c0a2c8ba920c78a1238d64f5a7cf8dcb3af04799
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-03-13 19:44:54 -0700

    Not being stupid

--- js/ButtonCount.js
+++ js/ButtonCount.js
@@ -1,6 +1,12 @@
 
 class ButtonCount extends HTMLElement {
     
+    UpdateButton()
+    {
+        this.timesClicked += 1;
+        targetButton.value = `Times Clicked: ${this.timesClicked}`;
+    }
+
     constructor() {
         super();
         this.timesClicked = 0;
@@ -8,14 +14,9 @@ class ButtonCount extends HTMLElement {
         //Attach a button that counts
         this.shadowRoot.innerHTML = '<input type="button" id="buttonClick" value="Times Clicked: 0">'
         this.targetButton = this.shadowRoot.getElementById('buttonClick');
-        this.shadowRoot.getElementById('buttonClick').addEventListener('click', UpdateButton);
+        this.shadowRoot.getElementById('buttonClick').addEventListener('click', this.UpdateButton);
     }
 
-    UpdateButton()
-    {
-        this.timesClicked += 1;
-        targetButton.value = `Times Clicked: ${this.timesClicked}`;
-    }
 }
 
 window.customElements.define('button-count', ButtonCount);
