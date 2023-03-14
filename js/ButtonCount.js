91415c4e4e4ed9965f74be184047b42dda3b6a66
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-03-13 19:32:40 -0700

    Better storage?

--- js/ButtonCount.js
+++ js/ButtonCount.js
@@ -1,9 +1,9 @@
 let timesClicked = 0;
-
+let targetButton;
 function UpdateButton()
 {
-    timesClicked++;
-    document.getElementById('buttonClick').value = `Times Clicked: ${timesClicked}`;
+    timesClicked += 1;
+    targetButton.value = `Times Clicked: ${timesClicked}`;
 }
 
 class ButtonCount extends HTMLElement {
@@ -12,6 +12,7 @@ class ButtonCount extends HTMLElement {
         this.attachShadow({mode:'open'});
         //Attach a button that counts
         this.shadowRoot.innerHTML = '<input type="button" id="buttonClick" value="Times Clicked: 0">'
+        targetButton = this.shadowRoot.getElementById('buttonClick');
         this.shadowRoot.getElementById('buttonClick').addEventListener('click', UpdateButton);
     }
 }
