1c1ac7ec44d6451d30c96ddcc051e7a9b99011ce
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-03-13 19:41:22 -0700

    Placed everything in a nice class

--- js/ButtonCount.js
+++ js/ButtonCount.js
@@ -1,19 +1,20 @@
-let timesClicked = 0;
-let targetButton;
-function UpdateButton()
-{
-    timesClicked += 1;
-    targetButton.value = `Times Clicked: ${timesClicked}`;
-}
 
 class ButtonCount extends HTMLElement {
+    
     constructor() {
+        this.timesClicked = 0;
         super();
         this.attachShadow({mode:'open'});
         //Attach a button that counts
         this.shadowRoot.innerHTML = '<input type="button" id="buttonClick" value="Times Clicked: 0">'
-        targetButton = this.shadowRoot.getElementById('buttonClick');
+        this.targetButton = this.shadowRoot.getElementById('buttonClick');
         this.shadowRoot.getElementById('buttonClick').addEventListener('click', UpdateButton);
+    }
+
+    UpdateButton()
+    {
+        this.timesClicked += 1;
+        targetButton.value = `Times Clicked: ${this.timesClicked}`;
     }
 }
 
