db78067f528cd778c61ad1e886bb8d87d8de31a5
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-03-13 19:26:11 -0700

    Added component

---
+++ js/ButtonCount.js
@@ -0,0 +1,19 @@
+let timesClicked = 0;
+
+function UpdateButton()
+{
+    timesClicked++;
+    document.getElementById('buttonClick').value = `Times Clicked: ${timesClicked}`;
+}
+
+class ButtonCount extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode:'open'});
+        //Attach a button that counts
+        this.shadowRoot.innerHTML = '<button id="buttonClick" value="Times Clicked: 0">'
+        document.getElementById('buttonClick').addEventListener('click', UpdateButton);
+    }
+}
+
+window.customElements.define('button-count', ButtonCount);
