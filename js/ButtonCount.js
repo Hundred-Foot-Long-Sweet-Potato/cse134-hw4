3005eb26274c1d4e85a57dcb175503c620040a82
Eric <60043853+Hundred-Foot-Long-Sweet-Potato@users.noreply.github.com>
2023-03-13 20:08:22 -0700

    Actually adding everything in one constructor

--- js/ButtonCount.js
+++ js/ButtonCount.js
@@ -7,16 +7,11 @@ class ButtonCount extends HTMLElement {
         this.attachShadow({mode:'open'});
         //Attach a button that counts
         this.shadowRoot.innerHTML = '<input type="button" id="buttonClick" value="Times Clicked: 0">'
-        this.shadowRoot.getElementById('buttonClick').addEventListener('click', this.UpdateButton);
+        this.shadowRoot.getElementById('buttonClick').addEventListener('click', ()=>{
+            this.timesClicked += 1;
+            this.shadowRoot.getElementById('buttonClick').value = `Times Clicked: ${this.timesClicked}`;
+        });
     }
-    
-    UpdateButton()
-    {
-        this.timesClicked += 1;
-        this.shadowRoot.getElementById('buttonClick').value = `Times Clicked: ${this.timesClicked}`;
-    }
-
-
 }
 
 window.customElements.define('button-count', ButtonCount);
